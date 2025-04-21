442e43ff688e3789f093dae0c5f70ebd6dd336bf
Sarwar <macbookair@Sarwar-2.local>
2025-04-22 04:14:11 +0600

    done setup for single page, adjusted archive css files

--- templates/single.php
+++ templates/single.php
@@ -1,14 +1,37 @@
 <?php include plugin_dir_path(__FILE__) . 'header.php'; ?>
 
-<div class="sd-single-post">
-    <?php
-    if (have_posts()) :
-        while (have_posts()) : the_post();
-            the_title('<h1 class="sd-title">', '</h1>');
-            the_content();
-        endwhile;
-    endif;
-    ?>
-</div>
+<main class="sd-single">
+    <section class="sd-container">
+
+        <div class="sd-single-page-container">
+
+            <div class="sd-single-wrapper">
+
+                <!-- Single header -->
+                <div class="sd-single-header">
+                    <h1><?php the_title(); ?></h1>
+                </div>
+
+                <!-- Business details starts here -->
+                <div class="sd-business-details">
+                    <?php if (have_posts()) : ?>
+                        <?php while (have_posts()) : the_post(); ?>
+                            <?php include plugin_dir_path(__FILE__) . 'content-single.php'; ?>
+                        <?php endwhile; ?>
+                    <?php else : ?>
+                        <p>No Business found.</p>
+                    <?php endif; ?>
+                </div>
+
+            </div> <!-- sd-single-wrapper -->
+
+            <div class="sd-sidebar" style="border: 1px solid #ccc; padding: 20px; background-color: #f9f9f9;">
+                Sidebar
+            </div>
+
+        </div> <!-- sd-single-page-container -->
+
+    </section>
+</main>        
 
 <?php include plugin_dir_path(__FILE__) . 'footer.php'; ?>
