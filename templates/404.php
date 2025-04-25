f2995abcf398f804493c7b0c3368e9bb076acfa8
Sarwar <macbookair@Sarwar-2.local>
2025-04-25 13:08:19 +0600

    added 404 template

--- templates/404.php
+++ templates/404.php
@@ -1,10 +1,12 @@
-<?php
-// bip-network-kit/templates/404.php
-get_header(); ?>
-<main class="sd-404">
+<?php get_header(); ?>
+<main class="sd-404" style="height:100vh;width:100vw;margin:auto;display:flex;flex-direction:column;justify-content:center;align-items:center;background:#000;color:#fff">
   <h1>Page Not Found</h1>
   <p>Sorry, we couldn’t find what you’re looking for.</p>
-  <a href="<?php echo esc_url( home_url() ); ?>">Return home</a>
+  <a href="<?php echo esc_url( home_url() ); ?>" style="background:#2f2f2f;padding:10px 30px;border-radius:10px;display:block;">Return home</a>
 </main>
-<?php
-get_footer();
+<style>
+    header,footer {
+        display:none !important;
+    }
+</style>
+<?php get_footer(); ?>
