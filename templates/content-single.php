8921b39680ff0a5ca6e4bcd3ea5d1d72f0954a52
Sarwar <macbookair@Sarwar-2.local>
2025-05-03 18:06:06 +0600

    added google ad for single and archive page

--- templates/content-single.php
+++ templates/content-single.php
@@ -1,6 +1,21 @@
 <div><?php echo do_shortcode( '[sd_breadcrumb]' ); ?></div>
 <?php echo do_shortcode( '[sd_single_banner]' ); ?>
 
+<div class="sd-google-ad full">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4744853284629324"
+        crossorigin="anonymous"></script>
+    <!-- Network -->
+    <ins class="adsbygoogle"
+        style="display:block"
+        data-ad-client="ca-pub-4744853284629324"
+        data-ad-slot="1514083567"
+        data-ad-format="auto"
+        data-full-width-responsive="true"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+</div>
+
 <section>
 
     <div><?php echo do_shortcode( '[sd_single_features]' ); ?></div>
